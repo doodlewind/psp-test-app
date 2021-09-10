351a699a86551e16415c8852a57723a0388dd6aa
Yifeng Wang <i@ewind.us>
2021-09-10 17:13:53 +0800

    chore: add realloc test case

--- libdemo-sys/src/demo.c
+++ libdemo-sys/src/demo.c
@@ -3,11 +3,8 @@
 void my_print(int a);
 
 void test_alloc() {
-  void* p = malloc(1000);
+  void* p = malloc(80);
+  p = realloc(p, 88);
 
-  int a = (int)realloc(p, 20);
-  my_print(a);
-
-  int b = (int)realloc(p, 88);
-  my_print(b);
+  my_print((int)p);
 }
